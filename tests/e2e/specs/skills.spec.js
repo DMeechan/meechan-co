2327383d430afd3c7ec5d17afe79777cf095b6ca
Daniel Meechan <daniel.meechan.1@gmail.com>
2018-07-29 01:40:30 +0100

    Added skills content tests

--- tests/e2e/specs/skills.spec.js
+++ tests/e2e/specs/skills.spec.js
@@ -6,4 +6,15 @@ describe('Skills page', () => {
     it('contains title', () => {
         cy.contains('h1.title', "And what I'm good at");
     });
+
+    it('contains all 8 skills', () => {
+        cy.contains('Node.js');
+        cy.contains('Meteor.js');
+        cy.contains('JavaScript');
+        cy.contains('Vue.js');
+        cy.contains('Git');
+        cy.contains('Infrastructure Automation');
+        cy.contains('Automated Testing');
+        cy.contains('Artificial Intelligence');
+    })
 });
